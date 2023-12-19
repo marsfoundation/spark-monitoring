1ce1af1fa3691d71024d4e68ad126b88e5b30dbd
Bartek Rutkowski <barrutko.eth@gmail.com>
2023-12-19 17:57:00 +0100

    refactor: replace tab with spaces

--- actions/src/utils/formatBigInt.ts
+++ actions/src/utils/formatBigInt.ts
@@ -2,6 +2,6 @@ export function formatBigInt(value: any, decimals: any): string {
     const integerPart = BigInt(value) / BigInt(10 ** decimals);
     const fractionalPart = BigInt(value) % BigInt(10 ** decimals);
     const fractionalString = fractionalPart.toString().padStart(decimals, '0');
-	const integerPartString = integerPart.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    const integerPartString = integerPart.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
     return `${integerPartString}.${fractionalString}`;
 }
