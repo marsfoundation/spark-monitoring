50b060ba0968c35d424b0275690ce49163f9197d
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-01-23 12:30:12 -0600

    feat: fetch all assets data

--- actions/src/getLiquidationSparkLend.ts
+++ actions/src/getLiquidationSparkLend.ts
@@ -10,14 +10,17 @@ import {
 } from 'ethers'
 
 import {
+    oracleAbi,
     poolAbi,
 } from './abis'
 
 import {
     createMainnetProvider,
+    fetchAllAssetsData,
 } from './utils'
 
 const SPARKLEND_POOL = '0xC13e21B648A5Ee794902342038FF3aDAB66BE987'
+const SPARKLEND_ORACLE = "0x8105f69D9C41644c6A0803fDA7D03Aa70996cFD9"
 
 export const getLiquidationSparkLend: ActionFn = async (context: Context, event: Event) => {
 	const txEvent = event as TransactionEvent
@@ -25,11 +28,19 @@ export const getLiquidationSparkLend: ActionFn = async (context: Context, event:
     const provider = await createMainnetProvider(context)
 
     const pool = new Contract(SPARKLEND_POOL, poolAbi, provider)
+	const oracle = new Contract(SPARKLEND_ORACLE, oracleAbi, provider)
 
-    const filteredParsedLiquidationLogs = txEvent.logs
+    const liquidationLogs = txEvent.logs
         .filter(log => log.address.toLowerCase() == SPARKLEND_POOL.toLowerCase())
         .map(log => pool.interface.parseLog(log))
         .filter(log => log?.name == 'LiquidationCall')
 
-    console.log({filteredParsedLiquidationLogs})
+    liquidationLogs.forEach(log => {
+        console.log({log})
+    })
+
+    if (liquidationLogs[0]) {
+        console.log(await fetchAllAssetsData(liquidationLogs[0].args[5], pool, oracle, provider))
+    }
+
 }
--- actions/src/utils/index.ts
+++ actions/src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './createMainnetProvider'
+export * from './fetchAllAssetsData'
 export * from './formatBigInt'
 export * from './sendMessagesToPagerDuty'
 export * from './sendMessagesToSlack'
