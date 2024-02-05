01d281d6f9af813ef93ba5cf47f5739f9ee3f39b
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-02-05 11:54:24 -0600

    feat: use json storage

--- actions/src/utils/transactionAlreadyProcessed.ts
+++ actions/src/utils/transactionAlreadyProcessed.ts
@@ -3,12 +3,19 @@ import {
 	TransactionEvent,
 } from '@tenderly/actions'
 
-export const transactionAlreadyProcessed = async (actionName: string, context: Context, txEvent: TransactionEvent) => {
-    if (await context.storage.getNumber(`${actionName}-${txEvent.hash}`) == 1) {
+export const transactionAlreadyProcessed = async (actionName: string, context: Context, txEvent: TransactionEvent): Promise<boolean> => {
+    const processedTransactionsRegistry = await context.storage.getJson(`${actionName}-tx-registry`) || {}
+
+    if (processedTransactionsRegistry[txEvent.hash]) {
         console.log(`Transaction ${txEvent.hash} was already processed by action ${actionName}`)
         return true
     }
-    await context.storage.putNumber(`${actionName}-${txEvent.hash}`, 1)
+
     console.log(`Transaction ${txEvent.hash} is being saved as processed by action ${actionName}`)
+    await context.storage.putJson(`${actionName}-tx-registry`, {
+        ...processedTransactionsRegistry,
+        [txEvent.hash]: 1,
+    })
+
     return false
 }
