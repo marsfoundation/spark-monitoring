1f549e9643ed9bb6a435882f69038aea3fa30306
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-02-05 13:10:19 -0600

    feat: add cleanTransactionRegistry

---
+++ actions/src/cleanTransactionRegistry.ts
@@ -0,0 +1,30 @@
+import {
+	ActionFn,
+	BlockEvent,
+	Context,
+	Event,
+} from '@tenderly/actions'
+
+export const cleanTransactionRegistry: ActionFn = async (context: Context, event: Event) => {
+    const blockEvent = event as BlockEvent
+
+    await removeStaleRecordsFromRegistry('getConfigurationChangeAave', blockEvent, context)
+    await removeStaleRecordsFromRegistry('getHighGasTransaction', blockEvent, context)
+    await removeStaleRecordsFromRegistry('getLiquidationSparkLend', blockEvent, context)
+    await removeStaleRecordsFromRegistry('getProtocolInteractionSparklend', blockEvent, context)
+}
+
+const removeStaleRecordsFromRegistry = async (actionName: string, blockEvent: BlockEvent, context: Context) => {
+    const registry = await context.storage.getJson(`${actionName}-tx-registry`)
+
+    if(!registry) return
+
+    await context.storage.putJson(`${actionName}-tx-registry`, Object.keys(registry as Record<string, number>).reduce((updatedRegistry, txHash) => {
+        if (registry[txHash] + 100 > blockEvent.blockNumber) {
+            updatedRegistry[txHash] = registry[txHash]
+        } else {
+            console.log(`Removing tx ${txHash} at ${registry[txHash]} from ${actionName}`)
+        }
+        return updatedRegistry
+    }, {} as Record<string, number>))
+}
