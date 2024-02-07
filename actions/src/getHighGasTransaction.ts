b6676b16b9ecff9790a09d5fcd5d6765018a75d4
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-02-07 17:15:14 -0600

    feat: add percentage based gas tip deviance threshold

--- actions/src/getHighGasTransaction.ts
+++ actions/src/getHighGasTransaction.ts
@@ -9,11 +9,13 @@ import {
     createEtherscanTxLink,
     createMainnetProvider,
     formatBigInt,
+    getDevianceInBasisPoints,
     sendMessagesToSlack,
     transactionAlreadyProcessed,
 } from './utils'
 
 export const HIGH_GAS_TRANSACTION_THRESHOLD = 2_500_000n as const
+export const GAS_PRICE_DEVIANCE_THRESHOLD = 1000 as const
 
 export const getHighGasTransaction: ActionFn = async (context: Context, event: Event) => {
 	const transactionEvent = event as TransactionEvent
@@ -21,15 +23,26 @@ export const getHighGasTransaction: ActionFn = async (context: Context, event: E
     const provider = await createMainnetProvider(context)
 
     const block = await provider.getBlock(transactionEvent.blockNumber)
+    if (!block) return
     console.log({block})
+
     const receipt = await provider.getTransactionReceipt(transactionEvent.hash)
+    if (!receipt) return
     console.log({receipt})
-    const tx = await provider.getTransaction(transactionEvent.hash)
-    console.log({tx})
+
+    const baseFee = block.baseFeePerGas
+    if (!baseFee) return
+
+    const paidFee = receipt.gasPrice
+
+    const deviance = getDevianceInBasisPoints(baseFee, paidFee)
 
     if (await transactionAlreadyProcessed('getHighGasTransaction', context, transactionEvent)) return
 
-    if (BigInt(transactionEvent.gasUsed) >= HIGH_GAS_TRANSACTION_THRESHOLD) {
+    if (
+        BigInt(transactionEvent.gasUsed) >= HIGH_GAS_TRANSACTION_THRESHOLD
+        && deviance >= GAS_PRICE_DEVIANCE_THRESHOLD
+    ) {
         await sendMessagesToSlack([`\`\`\`
 🚨🔥 HIGH GAS TRANSACTION 🚨🔥
 
