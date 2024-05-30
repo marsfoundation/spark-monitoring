2ed25d39d846db38ce81280f419f6d60d593eadf
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-05-30 13:44:17 +0200

    feat: track arbitrum dsr oracle data sync (#31)
    * feat: track arbitrum dsr oracle data sync
    
    * fix: change tab to spaces

--- actions/src/getPotDsrDataSync.ts
+++ actions/src/getPotDsrDataSync.ts
@@ -14,6 +14,7 @@ import { createMainnetProvider, createProvider, sendMessagesToSlack } from './ut
 const MAKER_POT = '0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7' as const
 const OPTIMISM_DSR_AUTH_ORACLE = '0x33a3aB524A43E69f30bFd9Ae97d1Ec679FF00B64' as const
 const BASE_DSR_AUTH_ORACLE = '0x2Dd2a2Fe346B5704380EfbF6Bd522042eC3E8FAe' as const
+const ARBITRUM_DSR_AUTH_ORACLE = '0xE206AEbca7B28e3E8d6787df00B010D4a77c32F3' as const
 
 const DISCREPANCY_TIME_THRESHOLD = 3_600_000 as const  // 1 hour in milliseconds
 
@@ -23,10 +24,12 @@ export const getPotDsrDataSync: ActionFn = async (context: Context, event: Event
     const mainnet = await createMainnetProvider(context)
     const optimism = await createProvider(context, 'OPTIMISM_RPC_URL')
     const base = await createProvider(context, 'BASE_RPC_URL')
+    const arbitrum = await createProvider(context, 'ARBITRUM_RPC_URL')
 
-	const pot = new Contract(MAKER_POT, potAbi, mainnet)
+    const pot = new Contract(MAKER_POT, potAbi, mainnet)
     const optimismDsrAuthOracle = new Contract(OPTIMISM_DSR_AUTH_ORACLE, dsrAuthOracleAbi, optimism)
     const baseDsrAuthOracle = new Contract(BASE_DSR_AUTH_ORACLE, dsrAuthOracleAbi, base)
+    const arbitrumDsrAuthOracle = new Contract(ARBITRUM_DSR_AUTH_ORACLE, dsrAuthOracleAbi, arbitrum)
 
     const mainnetDsr = await pot.dsr()
     console.log({mainnetDsr})
@@ -35,12 +38,16 @@ export const getPotDsrDataSync: ActionFn = async (context: Context, event: Event
     console.log({optimismDsr})
     const [ baseDsr ] = await baseDsrAuthOracle.getPotData()
     console.log({baseDsr})
+    const [ arbitrumDsr ] = await arbitrumDsrAuthOracle.getPotData()
+    console.log({arbitrumDsr})
 
     mainnetDsr != optimismDsr && await handleDsrDiscrepancy('Optimism', mainnetDsr, optimismDsr, periodicEvent, context)
     mainnetDsr != baseDsr && await handleDsrDiscrepancy('Base', mainnetDsr, baseDsr, periodicEvent, context)
+    mainnetDsr != arbitrumDsr && await handleDsrDiscrepancy('Arbitrum', mainnetDsr, arbitrumDsr, periodicEvent, context)
 
     await cleanUpStaleDiscrepancyRecord('Optimism', periodicEvent, context)
     await cleanUpStaleDiscrepancyRecord('Base', periodicEvent, context)
+    await cleanUpStaleDiscrepancyRecord('Arbitrum', periodicEvent, context)
 }
 
 const handleDsrDiscrepancy = async (
