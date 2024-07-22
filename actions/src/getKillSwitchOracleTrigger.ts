f92c71d301019ef4cbd770272d38db3ffea0ae60
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-07-22 15:41:10 +0200

    feat: implement getKillSwitchOracleTrigger

---
+++ actions/src/getKillSwitchOracleTrigger.ts
@@ -0,0 +1,32 @@
+import {
+    ActionFn,
+    Context,
+    Event,
+    TransactionEvent,
+} from '@tenderly/actions'
+
+import { Contract } from 'ethers'
+
+import { killSwitchOracleAbi } from './abis'
+
+import { createEtherscanTxLink, createMainnetProvider, sendMessagesToSlack } from './utils'
+
+const KILL_SWITCH_ORACLE = '0x909A86f78e1cdEd68F9c2Fe2c9CD922c401abe82' as const
+
+export const getKillSwitchOracleTrigger: ActionFn = async (context: Context, event: Event) => {
+    const transactionEvent = event as TransactionEvent
+
+    const provider = await createMainnetProvider(context)
+    const killSwitchOracle = new Contract(KILL_SWITCH_ORACLE, killSwitchOracleAbi, provider)
+
+    const logs = transactionEvent.logs
+		.filter(log => log.address.toLowerCase() == KILL_SWITCH_ORACLE.toLowerCase())
+		.map(log => killSwitchOracle.interface.parseLog(log))
+		.filter(log => log && log.name === 'Trigger')
+        .map(log => log && `\nOracle:    ${log.args[0]}\nThreshold: ${log.args[1]}\nAsnwer:    ${log.args[2]}\n`)
+
+    await sendMessagesToSlack([`\`\`\`
+🚨🕹️💀 Trigger called on Kill Switch 🚨🕹️💀
+${logs.join('')}
+${createEtherscanTxLink(transactionEvent.hash)}\`\`\``], context, 'SPARKLEND_ALERTS_SLACK_WEBHOOK_URL')
+}
