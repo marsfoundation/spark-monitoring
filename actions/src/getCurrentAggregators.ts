8e659c7f60648ef3acd1667ad86be00a0410d539
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-07-26 13:52:33 +0200

    feat: polish getCurrentAggregators

--- actions/src/getCurrentAggregators.ts
+++ actions/src/getCurrentAggregators.ts
@@ -9,11 +9,11 @@ import { Contract } from 'ethers'
 
 import { priceSourceAbi } from './abis'
 
-import { createMainnetProvider, transactionAlreadyProcessed } from './utils'
+import { createMainnetProvider, sendMessagesToSlack, transactionAlreadyProcessed } from './utils'
 
 const priceSources = {
-    WBTC_BTC_ORACLE: '0xfdFD9C85aD200c506Cf9e21F1FD8dd01932FBB23',
-    STETH_ETH_ORACLE: '0x86392dC19c0b719886221c78AB11eb8Cf5c52812',
+    WBTC_BTC: '0xfdFD9C85aD200c506Cf9e21F1FD8dd01932FBB23',
+    STETH_ETH: '0x86392dC19c0b719886221c78AB11eb8Cf5c52812',
 } as const
 
 export const getCurrentAggregators: ActionFn = async (context: Context, event: Event) => {
@@ -27,7 +27,7 @@ export const getCurrentAggregators: ActionFn = async (context: Context, event: E
     for (const [name, address] of Object.entries(priceSources)) {
         const priceSource = new Contract(address, priceSourceAbi, provider)
         const aggregatorAddress = await priceSource.aggregator()
-        messageBits.push(`${name}: ${aggregatorAddress}\n(https://etherscan.io/address/${address}#readContract#F2)`)
+        messageBits.push(`${name} aggregator: ${aggregatorAddress}\nhttps://etherscan.io/address/${address}#readContract#F2`)
     }
             const message = [`\`\`\`
 ⛓️🔮 Oracle aggregators updated ⛓️🔮
@@ -35,8 +35,7 @@ export const getCurrentAggregators: ActionFn = async (context: Context, event: E
 Current aggregators:
 ${messageBits.join('\n\n')}
 
-Make sure that all KillSwitch Keepers are configured to monitor updated price aggregators\`\`\``]
+🚨🚨 Make sure that all KillSwitch Keepers are configured to monitor updated price aggregators 🚨🚨\`\`\``]
 
-console.log(message)
-// await sendMessagesToSlack(message, context, 'SPARKLEND_ALERTS_SLACK_WEBHOOK_URL')
+await sendMessagesToSlack(message, context, 'SPARKLEND_ALERTS_SLACK_WEBHOOK_URL')
 }
